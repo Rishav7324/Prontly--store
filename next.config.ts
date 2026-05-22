a7a4e45e3a07227a0da0d68ac3b20f26efbc7221
RISHAV RAJ <rishavraj74883@gmail.com>
2026-05-22 15:05:16 +0000

    Cloudflare R2 (Private Asset Storage) R2_ACCOUNT_ID=eec0abde65a90cda7879

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.prontly.in',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
